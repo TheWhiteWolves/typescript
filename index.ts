63b31f98cda6a8bfaedd5fb44323e94317521de9
TheWhiteWolves <jonsmith0@gmail.com>
2025-07-01 19:24:40 +0100

    add progress

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { log, chats } from "./chats"
+
+const button = document.getElementById("enable-button")!;
+
+button.addEventListener("click", () => {
+  // @ts-ignore
+  window.supportAI.enableAutoReply();
+});
+
+log(chats);
